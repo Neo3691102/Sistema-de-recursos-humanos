239a0eac72fde7730c072a5242707fb6796a25ff
Neo3691102 <nestor8772@gmail.com>
2026-02-25 14:35:09 -0600

    Estructura inicial del proyecto con dependencias spring web, driver de mysql, spring data con jpa y hibernate y lombok

---
+++ src/main/java/com/nestorcastaneda/proyectorh/ProyectorhApplication.java
@@ -0,0 +1,13 @@
+package com.nestorcastaneda.proyectorh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectorhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectorhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nestorcastaneda/proyectorh/ProyectorhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestorcastaneda.proyectorh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectorhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
