7b119190af9efd6fb2f085806c3bc2709213a9e8
Neo3691102 <nestor8772@gmail.com>
2026-03-03 21:22:43 -0600

    Se agrega el metodo agregarEmpleado para poder crear un nuevo registro en la base de datos.

--- src/main/java/com/nestorcastaneda/proyectorh/controlador/EmpleadoControlador.java
+++ src/main/java/com/nestorcastaneda/proyectorh/controlador/EmpleadoControlador.java
@@ -1,13 +1,14 @@
 package com.nestorcastaneda.proyectorh.controlador;
 
 
+import com.nestorcastaneda.proyectorh.entidad.Empleado;
 import com.nestorcastaneda.proyectorh.servicio.EmpleadoServicio;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("rh-app") //sirve para modificar la url, quedaría http://localhost8080/rh-app
@@ -17,4 +18,19 @@ public class EmpleadoControlador {
 
     @Autowired
     private EmpleadoServicio empleadoServicio;
+
+    //http:localhost:8080/empleados
+    @GetMapping("/empleados")
+    public List<Empleado> obtenerEmpleados(){
+        List<Empleado> empleados = empleadoServicio.listarEmpleados();
+        empleados.forEach((empleado) -> logger.info(empleado.toString()));
+        return empleados;
+    }
+
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
+        logger.info("Empleado a agregar: " + empleado);
+        return empleadoServicio.guardarEmpleado(empleado);
+    }
+
 }
